afaa3a2f094c86285896c94afa860db6b3ecb92a
Thai <thaiquocle1909200400@gmail.com>
2025-10-29 11:02:44 +0700

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
-import createSagaMiddleware from 'redux-saga'
-import authReducer from './slices/authSlice'
-import userReducer from './slices/userSlice'
-import patientReducer from './slices/patientSlice'
-import testOrderReducer from './slices/testOrderSlice'
-import instrumentReducer from './slices/instrumentSlice'
-import { rootSaga } from './sagas'
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import authReducer from "./slices/authSlice";
+import userReducer from "./slices/userSlice";
+import patientReducer from "./slices/patientSlice";
+import testOrderReducer from "./slices/testOrderSlice";
+import instrumentReducer from "./slices/instrumentSlice";
+import { rootSaga } from "./sagas";
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        users: userReducer,
-        patients: patientReducer,
-        testOrders: testOrderReducer,
-        instruments: instrumentReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            thunk: false,
-            serializableCheck: {
-                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-            },
-        }).concat(sagaMiddleware),
-})
+  reducer: {
+    auth: authReducer,
+    users: userReducer,
+    patients: patientReducer,
+    testOrders: testOrderReducer,
+    instruments: instrumentReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false,
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }).concat(sagaMiddleware),
+});
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
